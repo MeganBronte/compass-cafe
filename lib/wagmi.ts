c4bf4ac257c0afe653541689344dcd14877de267
Megan Bronte <161073266+MeganBronte@users.noreply.github.com>
2026-06-10 15:22:55 +0800

    Add build attribution code

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -5,8 +5,10 @@ import { base } from "wagmi/chains";
 export const contractAddress =
   "0x88fcc47bc0b908da1f0552f03bb436c4045248ff" as `0x${string}`;
 
+export const buildCode = "bc_dv5x3uxc";
+
 export const attributionDataSuffix =
-  "0x" as `0x${string}`;
+  "0x62635f64763578337578630b0080218021802180218021802180218021" as `0x${string}`;
 
 export const wagmiConfig = createConfig({
   chains: [base],
